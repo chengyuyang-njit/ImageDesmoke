2b606ef0b234105df162aa80d4faa7e44d5aca5f
chengyuyang-njit <cy322@njit.edu>
2025-03-10 09:30:26 -0400

    exp

--- utils/ls_ckp_config.py
+++ utils/ls_ckp_config.py
@@ -7,7 +7,7 @@ args = argparse.ArgumentParser(description="show configuration of checkpoint")
 args.add_argument('-p', '--PATH', default = None, type = str, required = True,
                       help = 'path to the checkpoint file')
 args = args.parse_args()
-
-ckp = torch.load(args.PATH)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+ckp = torch.load(args.PATH, map_location=device)
 
 print(json.dumps(ckp["config"], indent = 4))
